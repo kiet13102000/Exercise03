0694cd2ff7edd321047b84254d9feef86e9e67b8
kiet nguyen <kietnguyen20101010@gmail.com>
2019-09-25 20:11:21 +0700

    tesst

---
+++ Exercise03/Exercise03/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("heloo every body");
+        }
+    }
+}
